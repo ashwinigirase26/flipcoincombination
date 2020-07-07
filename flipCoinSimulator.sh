44425f11abae27d27c963236eed8013b7215d0aa
“Enter <ashwinigirase26@gmail.com>
2020-07-07 21:51:45 +0530

    loop for simulator

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,4 +10,27 @@ if [ $no -eq 1 ]
  else
 	echo "tails win $no"
   fi
+#loop for simulator
+hwin=0
+twin=0
+while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
+do
+	toss=$((RANDOM%2))
+	if [ $toss -eq 1 ]
+	then
+		echo "HEAD"
+		((hwin++))
+	else
+		echo "TAILS"
+		((twin++))
+	fi
+done
+echo "NO OF TIME HEAD WIN"$hwin
+echo "NO OF TIME TAIL WIN"$twin
 
+if [ $hwin -gt $twin ]
+then
+echo "HEAD WIN BY"$((hwin-twin))
+else
+echo "TAILS WIN BY"$((hwin-twin))
+fi
