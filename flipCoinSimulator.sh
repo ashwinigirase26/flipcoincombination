f630cd19734abb8fc793b9ff5a767ce865d50dc0
“Enter <ashwinigirase26@gmail.com>
2020-07-07 21:58:56 +0530

    modify

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -34,3 +34,30 @@ echo "HEAD WIN BY"$((hwin-twin))
 else
 echo "TAILS WIN BY"$((hwin-twin))
 fi
+
+#Modify the earlier UC 2 to continue till either of them have won 21 times. Show if it’s a Win
+#or Tie. If Win then who won and by how much
+echo " Modify the earlier UC 2 to continue till either of them have won 21 times. Show if it’s a Win or Tie. If Win then who won and by how much"
+for((i=1;i<60;i++))
+do
+        no=$((RANDOM%2))
+        if [ $no -eq 1 ]
+        then
+                (( heads ++ ))
+                echo "head count: $heads"
+        else
+                 (( tails ++ ))
+                 echo "tail count: $tails"
+        fi
+        if [ $heads -eq 21 ]
+        then
+                echo "heads win"
+                break
+        elif [ $tails -eq 21 ]
+        then
+                echo "tails win"
+                break
+        fi
+
+done
+
