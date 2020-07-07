0700672d48f7a04b02d10dd9f48cd6b692c3322d
“Enter <ashwinigirase26@gmail.com>
2020-07-07 21:47:55 +0530

    displays head or tail

---
+++ flipCoinSimulator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+echo "As a Simulator start with Flipping a Coin to Display Heads or tails"
+heads=0
+tails=0
+
+no=$((RANDOM%2))
+if [ $no -eq 1 ]
+  then
+	echo "head win $no"
+ else
+	echo "tails win $no"
+  fi
+
