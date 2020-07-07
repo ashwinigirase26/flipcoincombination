ccd3c131ebdf7a4cde2d30efa625214c6cacbeef
“Enter <ashwinigirase26@gmail.com>
2020-07-07 22:55:53 +0530

    singlet combination

--- flipCoincombination.sh
+++ flipCoincombination.sh
@@ -9,3 +9,28 @@ then
 else
         echo "Tail wins"
 fi
+
+##percentage of singlet combination
+
+read -p "ENTER NO OF Flip:" FLIP
+declare -A DIC
+DIC[H]=0
+DIC[T]=0
+for ((TURN=0;TURN<$FLIP;TURN++))
+do
+	COIN_SIDE=$((RANDOM % 2))
+	if [ $COIN_SIDE -eq 0 ]
+	then
+		VAL=${DIC[H]}
+		DIC[H]=$((VAL+1))
+	else
+		VAL=${DIC[T]}
+		DIC[T]=$((VAL+1))
+	fi
+done
+VAL=${DIC[H]}
+echo "Percent of heads is :" 
+echo "scale=2;$VAL/$FLIP*100"|bc 
+VAL=${DIC[T]}
+echo "Percent of tails is :" 
+echo "scale=2;$VAL/$FLIP*100"|bc
